f6d6b658056c1870d3a272671b2ddba0734e6a18
zritt <zritt>
2019-03-05 15:16:22 +0000

    CSVs and user.py updates

--- user.py
+++ user.py
@@ -12,31 +12,36 @@ class User():
 		
 class Student():
 	"""Object to store Student User information, create a new Student and let them login"""
-	
-	def __init__(self, inStudentNumber, inPassword, inUsername, inFirstname, inLastname):
-		self.studentUsername = inStudentNumber
-		self.password = inPassword
-		self.username = inUsername
-		self.firstname = inFirstname
-		self.lastname = inLastname
-	
-	def createNewStudent(self, inStudentNumber, inPassword, inUsername, inFirstname, inLastname):
+		
+	def createNewStudent(self,inUsername , inPassword, inFirstname, inLastname):
 		"""Add a new a student to the dB, using their student number as a key in the dB"""
 		
-		with open('students.csv', 'a') as csvfile:
+		with open('students.csv', 'a+') as csvfile:
 			# We are going to write an entire record into the csv file (append it)
 			linewriter = csv.writer(csvfile, delimiter=',', quotechar='"', 
 			quoting=csv.QUOTE_MINIMAL)
 			
+			self.username = inUsername
+			self.password = inPassword
+			self.firstname = inFirstname
+			self.lastname = inLastname
+			
+			self.studentNumber = self.count_row()
+			
 			# append the record to the csv file
-			linewriter.writerow([str(self.studentUsername), self.password, self.username, 
+			linewriter.writerow([int(self.studentNumber), self.username, self.password, 
 			self.firstname, self.lastname])
 			
-		
+	def count_row(self):
+		with open('students.csv', 'r') as csvfile:
+			linereader = csv.reader(csvfile)
+			row_count = sum(1 for line in linereader)
+			return row_count - 1
+	
 	def studentLogin(self, inStudentNumber, inPassword):
 		"""Allow the student to login, if the login credentials are correct
 		
-		>>> Student.studentLogin(Student, "000", "password")
+		>>> Student.studentLogin(Student, 0, "password")
 		True
 		"""
 		
@@ -49,45 +54,48 @@ class Student():
 			
 			# iterate over the records, and then access each element in the record
 			for line in linereader:
-				if str(inStudentNumber) == line[0]:
-					if str(inPassword) == line[2]:
+				if str(inStudentNumber) == str(line[0]):
+					if str(inPassword) == str(line[2]):
 						return True
 		return False
 		
 		
-	def __str__(self):
-		print("Student Username is: " + str(self.studentUsername) + "\nPassword is: " 
-		+ str(self.password))
-
-		
 class Tutor():
 	"""Object to store Tutor User information, create a new Tutor and let them login """
 	
-	def __init__(self, inTutorNumber, inPassword, inUsername, inFirstname, inLastname):
-		self.tutorUsername = inTutorNumber
-		self.password = inPassword
-		self.username = inUsername
-		self.firstname = inFirstname
-		self.lastname = inLastname
 	
-	def createNewTutor(self, inTutorNumber, inPassword, inUsername, inFirstname, inLastname):
-		"""Add a new a tutor to the dB, using their tutor number as a key in the dB"""
-		with open('tutors.csv', 'a') as csvfile:
+	def createNewTutor(self,inUsername , inPassword, inFirstname, inLastname):
+		"""Add a new a student to the dB, using their student number as a key in the dB"""
+		
+		with open('tutors.csv', 'a+') as csvfile:
 			# We are going to write an entire record into the csv file (append it)
 			linewriter = csv.writer(csvfile, delimiter=',', quotechar='"', 
 			quoting=csv.QUOTE_MINIMAL)
 			
+			self.username = inUsername
+			self.password = inPassword
+			self.firstname = inFirstname
+			self.lastname = inLastname
+			
+			self.tutorNumber = self.count_row()
+			
 			# append the record to the csv file
-			linewriter.writerow([str(self.tutorUsername), self.password, self.username, 
+			linewriter.writerow([int(self.tutorNumber), self.username, self.password, 
 			self.firstname, self.lastname])
-		
+			
+	def count_row(self):
+		with open('tutors.csv', 'r') as csvfile:
+			linereader = csv.reader(csvfile)
+			row_count = sum(1 for line in linereader)
+			return row_count - 1
+	
 	def tutorLogin(self, inTutorNumber, inPassword):
-		"""Allow the tutor to login, if the login credentials are correct
+		"""Allow the student to login, if the login credentials are correct
 		
-		>>> Tutor.tutorLogin(Tutor, "000", "password")
+		>>> Tutor.tutorLogin(Tutor, 0, "password")
 		True
 		"""
-
+		
 		with open('tutors.csv', 'r') as csvfile:
 			# place all the contents of the csv file into linereader
 			linereader = csv.reader(csvfile)
@@ -97,18 +105,19 @@ class Tutor():
 			
 			# iterate over the records, and then access each element in the record
 			for line in linereader:
-				if str(inTutorNumber) == line[0]:
-					if str(inPassword) == line[2]:
+				if str(inTutorNumber) == str(line[0]):
+					if str(inPassword) == str(line[2]):
 						return True
 		return False
 		
-		
-	def __str__(self):
-		print("Tutor Username is: " + str(self.tutorUsername) + "\nPassword is: " 
-		+ str(self.password))
-
+			
+	
+# def run():
+	# student_1 = Student()
+	# student_1.createNewStudent('test', 'password', 'testFName', 'TestLName')
 		
 # for testing purposes		
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+    #run()
