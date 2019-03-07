05d979406b219455054e5b435d3181d0768be227
noahgiles1 <mrnoahgiles@gmail.com>
2019-03-07 12:58:03 +0000

    Update report.py

--- report.py
+++ report.py
@@ -1,19 +1,64 @@
-# This class is for viewing reports of student performance by the tutor
-
 from tkinter import *
-import tkinter as tk
+import tkinter.ttk as ttk
 import csv
+import itertools
 
-class report(tk.Frame):
-    def __init__(self,master):
-        tk.Frame.__init__(self, master)
-        self.grid()
-        self.createWidgets()
+class Report(Frame):
 
-    def createWidgets(self):
-        pass
+    def __init__(self, master):
+
+        width = 1100
+        height = 400
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        x = (screen_width/2) - (width/2)
+        y = (screen_height/2) - (height/2)
+        root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+        root.resizable(0, 0)
 
 
-root = tk.Tk()
-app=report(root)
+        TableMargin = Frame(root, width=500)
+        TableMargin.pack(side=TOP)
+        scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
+        scrollbary = Scrollbar(TableMargin, orient=VERTICAL)
+        tree = ttk.Treeview(TableMargin, columns=("StudentID", "Firstname", "Lastname", "Testname", "Score"), height=400, selectmode="extended", yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+        scrollbary.config(command=tree.yview)
+        scrollbary.pack(side=RIGHT, fill=Y)
+        scrollbarx.config(command=tree.xview)
+        scrollbarx.pack(side=BOTTOM, fill=X)
+        tree.heading('StudentID', text="Student ID", anchor=W)
+        tree.heading('Firstname', text="Firstname", anchor=W)
+        tree.heading('Lastname', text="Lastname", anchor=W)
+        tree.heading('Testname', text="Testname", anchor=W)
+        tree.heading('Score', text="Score", anchor=W)
+        tree.column('#0', stretch=NO, minwidth=0, width=0)
+        tree.column('#1', stretch=NO, minwidth=0, width=200)
+        tree.column('#2', stretch=NO, minwidth=0, width=200)
+        tree.column('#3', stretch=NO, minwidth=0, width=300)
+        tree.pack()
+
+
+        f1 = open('students.csv')
+        f2= open('results.csv')
+        csv_f1 = csv.DictReader(f1, delimiter=',')
+        csv_f2 = csv.DictReader(f2, delimiter=',')
+
+        for row2, row1 in itertools.zip_longest(csv_f2, csv_f1):
+            if row2['Student ID'] == row1['stud id']:
+                Firstname = row1['first name']
+                Lastname = row1['last name']
+                StudentID = row2['Student ID']
+                Testname = row2['Test Name']
+                Score = row2['Score']
+                tree.insert("", 0, values=(StudentID, Firstname, Lastname, Testname, Score))
+
+
+
+
+
+
+#Main
+root = Tk()
+root.title("Report")
+app = Report(root)
 root.mainloop()
